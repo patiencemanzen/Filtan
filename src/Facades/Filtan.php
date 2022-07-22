62aec109456761fbf460b1262841c2c73e071b12
manirabona-programer <hseal419@gmail.com>
2022-07-22 10:25:14 +0200

    fixed service provider

---
+++ src/Facades/Filtan.php
@@ -0,0 +1,12 @@
+<?php
+    namespace Patienceman\Filtan\Facades;
+
+    use Illuminate\Support\Facades\Facade;
+
+    class Filtan extends Facade {
+
+        protected static function getFacadeAccessor(){
+
+            return 'Filtan';
+        }
+    }
